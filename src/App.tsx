87fa09b9195fabac4d86f358a877022fb6bb2420
Goutham Shibulal <sreelalgoutham.lootz@gmail.com>
2026-02-16 16:41:47 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Tech POC Showcase</h1>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Modern interactive showcase for tech POCs
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
